57d6da75c228b77d404c6c8b1b802b9fcb8aeab1
codingruo <r.schober@outlook.com>
2022-10-31 07:35:27 +0100

    Blog Post with Koa done

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const Koa = require("koa");
 const initDB = require("./db");
 const router = require("./middleware/router");
+const views = require("koa-views");
+const session = require("koa-session");
+const bodyParser = require("koa-bodyparser");
+const methodOverride = require("./middleware/method-override");
 
 // Initialize Database
 initDB();
 
 // Create new Koa Instance
 const app = new Koa();
+
+app.keys = ["secret key"];
+
+// Views
+app.use(views(`${__dirname}/views`, { extension: "ejs" }));
+
+// Session
+app.use(session(app));
+app.use(bodyParser());
+app.use(methodOverride());
 
 // Router
 app.use(router.routes());
--- middleware/router.js
+++ middleware/router.js
@@ -1,6 +1,40 @@
 const KoaRouter = require("koa-router");
+const authController = require("../controllers/AuthController");
+const postController = require("../controllers/PostController");
+const authenticated = require("./authenticated");
+const guest = require("./guest");
+const user = require("./user");
 
 const router = new KoaRouter();
-router.get("/", (ctx) => (ctx.body = "Welcome to the Koa Blog!!"));
+
+router.use(user());
+
+// auth routes
+const auth = new KoaRouter()
+  .get("/", guest(), authController.index)
+  .post("/login", authController.login)
+  .post("/register", authController.register)
+  .get("/logout", authController.logout);
+router.use("/auth", auth.routes());
+
+// blog post routes
+const posts = new KoaRouter();
+posts
+  .use(authenticated())
+  .post("/", postController.store)
+  .get("/create", postController.create)
+  .put("/:id", postController.update)
+  .get("/:id/edit", postController.edit);
+router.use("/post", posts.routes());
+
+// base routes
+// authenticated not required
+router
+    .get("/", postController.index)
+    .get("/post/:id", postController.show);
+
+
+
+
 
 module.exports = router;
